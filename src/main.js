f0fe0a0f1ce6ac2edf8b098f5ac764aaada547e3
Erik Mau <erik.mau@tngtech.com>
2024-11-15 11:01:38 +0100

    adjusting personal info

--- src/main.js
+++ src/main.js
@@ -4,26 +4,25 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'Christof Happ',
+		name: 'Erik Mau',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Red tiles on a screen on the wall',
 			'Scripts in strange programming languages',
-			'Some annoying technology preventing me to finish my ticket and breaking production'
+			'Some mysterious technology preventing me from breaking production code'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: ':sadpanda:',
+		tools: ':tekton:',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'Share knowledge about the basics of CI/CD'
+			'gain knowledge about the basics of CI/CD'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
 			'reading books', 
-			'cooking', 
+			'volleyball',
 			'playing computer games'
 		]
 	}
